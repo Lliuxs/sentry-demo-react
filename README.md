80aa3988c077bb4726d3802c8aa55427cc2852e4
xueshuai.liu <xueshuai.liu@shopee.com>
2022-04-13 18:22:23 +0800

    sentryplugin

--- README.md
+++ README.md
@@ -4,4 +4,17 @@ curl https://sentry.io/api/0/organizations/shopee-ads/releases/production@2022_0
  -d '{"environment":"production"}'
 
 
-sentry-cli releases set-commits --auto production@2022_04_13_4
+// auto 标志自动确定存储库名称
+sentry-cli releases -o $(SENTRY_ORG) -p $(SENTRY_PROJECT) set-commits --auto $(REACT_APP_RELEASE_VERSION)
+sentry-cli releases -o shopee-ads -p react-demo set-commits --auto  production@2022_04_13_4
+
+
+create_release:
+sentry-cli releases -o $(SENTRY_ORG) new -p $(SENTRY_PROJECT) $(REACT_APP_RELEASE_VERSION)
+
+upload_sourcemaps:
+    sentry-cli releases -o $(SENTRY_ORG) -p $(SENTRY_PROJECT) files $(REACT_APP_RELEASE_VERSION) \
+        upload-sourcemaps --url-prefix "~/static/js" --validate build/static/js
+
+ sentry-cli releases -o shopee-ads -p react-demo files production@2022_04_13_4 \
+        upload-sourcemaps --url-prefix "~/static/js" --validate build/static/js
--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,22 @@
+const dayjs = require('dayjs')
+const environment = 'production';
+const release = `${environment}@${dayjs().format('YYYY_MM_DD')}_4`;
 const { override } = require('customize-cra');
 // https://www.npmjs.com/package/@sentry/webpack-plugin
 const SentryCliPlugin = require('@sentry/webpack-plugin');
+
 
 // https://github.com/getsentry/sentry-webpack-plugin
 const addSentryPlugin = () => (config) => {
   config.plugins.push(
     new SentryCliPlugin({
-      // release: '1.0.0',
+      release: release,
       authToken:
-        '4c979c29b1794e86ab10f320193cb7f7efe43aacc10740dfa243ab7ae801635a',
-      url: 'http://127.0.0.1:9000',
+        '7abcd4c9b1df4380a604a8d313478eec315d23fe2e16422fb4e058e36970d5e0',
+      // url: 'http://127.0.0.1:9000',
       // url: 'https://sentry.io',
-      org: 'shopee',
-      project: 'react',
+      // org: 'shopee',
+      // project: 'react',
       urlPrefix: '~/',
       include: './build',
       ignore: ['node_modules'],
@@ -22,7 +26,7 @@ const addSentryPlugin = () => (config) => {
 };
 
 module.exports = {
-  // webpack: override(addSentryPlugin()),
+  webpack: override(addSentryPlugin()),
   // devServer: {},
 };
 
